962f3fddeefb7930764a3ed3a620e5d86007031d
Jess Telford <jtelford@groupon.com>
2014-12-29 12:06:34 +1100

    Simplify the test setup

--- test/setup.js
+++ test/setup.js
@@ -1,14 +1,4 @@
 var jsdom = require('jsdom');
 
-before('DOM Setup', function() {
-
-  // A super simple DOM ready for React to render into
-  // Store this DOM and the window ready for React to access
-  global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-  global.window = global.document.parentWindow;
-});
-
-after('DOM teardown', function() {
-  // We're done, so prevent memory leaks by closing the window
-  global.window.close();
-});
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.parentWindow;
